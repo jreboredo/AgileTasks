3a1d01f441602e5d697a145b45eb7058d7ebb52b
seba.cuccorese@gmail.com <lea123seba>
2020-10-18 18:20:34 -0300

    Init proyecto

---
+++ src/main/java/Elementos/Ing/AgileTasks/AgileTasksApplication.java
@@ -0,0 +1,13 @@
+package Elementos.Ing.AgileTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileTasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Elementos/Ing/AgileTasks/AgileTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package Elementos.Ing.AgileTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
