26f8bb335f385379e7c3ff51fc31db1cb4c9ee38
fagustin07 <federico.sandoval@alu.unq.edu.ar>
2020-10-28 15:30:56 -0300

    Navbar refactor

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,12 +7,12 @@ export default function NavBar() {
     return (
         <>
         <nav className="navbar navbar-expand-lg bg-dark rounded justify-content-between">
-            <div className='bg-dark rounded-lg'>
-                <h1 className='navbar-brand text-light p-3 mx-5 '>
-                    <span className='logo' onClick={() => history.push('/')}> Agile Tasks</span></h1>
+            <div className='bg-dark rounded-lg mb-3'>
+                <h1 className='text-light p-3 mx-5 '>
+                    <span className='logo' onClick={() => history.push('/home')}> Agile Tasks</span></h1>
             </div>
             <div>
-                <h3 className='text-light' onClick={() => history.push('/')}> Log out</h3>
+                <h3 className='text-light pointer' onClick={() => history.push('/')}> Log out</h3>
             </div>
         </nav>
         </>
