1e886ec925e171df8b27802231676fa2ad571f28
seba.cuccorese@gmail.com <lea123seba>
2020-11-06 19:06:00 -0300

    Se arreglaron los problemas sobre tarea y usario, Se agregaron funcionalidades al TareaController.kt

--- src/main/java/Elementos/Ing/AgileTasks/modelo/Usuario.java
+++ src/main/java/Elementos/Ing/AgileTasks/modelo/Usuario.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Usuario {
 
     //@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+   // @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private int id;
     @Id
