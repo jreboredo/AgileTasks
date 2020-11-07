1298edc931e52beabd32d0395448f4d858add2a0
fagustin07 <federico.sandoval@alu.unq.edu.ar>
2020-11-07 03:08:06 -0300

    change colors in prio

--- frontend/src/components/tasks/Task.jsx
+++ frontend/src/components/tasks/Task.jsx
@@ -7,16 +7,16 @@ import pin from '../../img/pin.png'
 
 export default function Task({task, editTask,showRemoveTask}){
     const {titulo,descripcion,prioridad,comienzo,fin} = task;
-  
+    const prio = prioridad==="low" ? 2 : prioridad==="med" ? 1 : 0
     return (
-        <div className={`task task--${prioridad}`}>
+        <div className={`task task--${prio}`}>
             <img src={pin} className="pin" alt='pin'/>
             <div className="actions">
                 <img src={edit} alt="edit" className="icon" onClick={() => editTask(task)}/>
                 <img src={remove} alt="edit" className="icon" onClick={() => showRemoveTask(task)}/>
             </div>
+            <h1 className="title">[TASK]</h1>
             <h3 className="title">{titulo}</h3>
-            <p className="text">{descripcion}</p>
         </div>
     );
 
